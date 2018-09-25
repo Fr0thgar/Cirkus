b71cfbc4560257217eca5f47d674837b3374d1be
Fr0thgar <jona270r@edu.easj.dk>
2018-09-25 09:15:23 +0200

    Add project files.

---
+++ Cirkus/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cirkus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
